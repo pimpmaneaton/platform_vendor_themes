df90b7d76f8b6d86278a59d2931397499d39aaa8
Jackeagle <jackeagle102@gmail.com>
2019-05-26 16:39:38 +0200

    vendor/themes: Build Themes
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: If8219149d0770191688137dce89f0631420394a1

---
+++ bliss_themes.mk
@@ -0,0 +1,130 @@
+#
+# Copyright (C) 2018-2019 The BlissRoms Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Misc Themes
+PRODUCT_PACKAGES += \
+    NotificationsDark \
+    NotificationsLight \
+    DocumentsUITheme \
+    NotificationsPrimary \
+    NotificationsBlack \
+    DialerTheme \
+    TelecommTheme \
+    DocumentsUITheme
+
+# Fonts
+PRODUCT_PACKAGES += \
+    BlissFonts
+
+# Accent Themes
+PRODUCT_PACKAGES += \
+    AccentSluttyPink \
+    AccentPixel \
+    AccentGoldenShower \
+    AccentDeepOrange \
+    AccentWhite \
+    AccentTeal \
+    AccentFromHell \
+    AccentBlueMonday \
+    AccentSmokingGreen \
+    AccentDeadRed \
+    AccentRottenOrange \
+    AccentDeepPurple \
+    AccentBliss \
+    AccentCandyRed \
+    AccentExtendedGreen \
+    AccentJadeGreen \
+    AccentPaleBlue \
+    AccentPaleRed \
+    AccentObfusBleu \
+    AccentNotImpPurple \
+    AccentHolillusion \
+    AccentMoveMint \
+    AccentFootprintPurple \
+    AccentBubblegumPink \
+    AccentFrenchBleu \
+    AccentManiaAmber \
+    AccentMisticBrown \
+    AccentSeasideMint \
+    AccentDreamyPurple \
+    AccentSpookedPurple \
+    AccentHeirloomBleu \
+    AccentTruFilPink \
+    AccentWarmthOrange \
+    AccentColdBleu \
+    AccentDiffDayGreen \
+    AccentDuskPurple \
+    AccentBurningRed \
+    AccentHazedPink \
+    AccentColdYellow \
+    AccentNewHouseOrange \
+    AccentIllusionsPurple \
+    AccentFlare \
+    AccentSublime \
+    AccentSincityRed \
+    AccentOrangeCoral \
+    AccentPureLust \
+    AccentQuepal \
+    AccentShadesOfGrey
+
+# Primary Themes
+PRODUCT_PACKAGES += \
+    PrimaryAlmostBlack \
+    PrimaryBlack \
+    PrimaryBliss \
+    PrimaryBlissClear \
+    PrimaryEyeSoother \
+    PrimaryWhite \
+    PrimaryColdWhite \
+    PrimaryWarmWhite \
+    PrimaryDarkBlue
+
+# QS tile styles
+PRODUCT_PACKAGES += \
+    QSTileDefault \
+    QSTileCircleTrim \
+    QSTileCircleDualTone \
+    QSTileCircleGradient \
+    QSTileCircleOutline \
+    QSTileCookie \
+    QSTileDottedCircle \
+    QSTileDualToneCircle \
+    QSTileInk \
+    QSTileInkdrop \
+    QSTileMountain \
+    QSTileNinja \
+    QSTileOreo \
+    QSTileOreoCircleTrim \
+    QSTileOreoSquircleTrim \
+    QSTilePokesign \
+    QSTileSquaremedo \
+    QSTileSquircle \
+    QSTileSquircleTrim \
+    QSTileTeardrop \
+    QSTileWavey
+
+# Themes
+#PRODUCT_PACKAGES += \
+#    Stock \
+#    DarkBlissTheme \
+#    BlackBlissTheme \
+#    PixelTheme \
+#    SluttyPinkTheme \
+#    DarknessMeisterTheme \
+#    SmokedGreenTheme \
+#    AmberTheme \
+#    Bl4ckAndYell0Theme \
+#    GboardDarkTheme
